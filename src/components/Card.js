adf80988973723124432ca995b4192aee395e543
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-23 23:11:12 -0300

    :construction: buscando repositórios contendo a string da rinha

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,18 +6,41 @@ function Card(props) {
   console.log('userFork: ', props.userFork);
 
   useEffect(() => {
+    // async function fetchUserRepository() {
+    //   try {
+    //     const repoUrl = `https://api.github.com/repos/${props.userLogin}/rinha-de-backend-2023-q3`;
+    //     const response = await fetch(repoUrl);
+    //     const repositoryData = await response.json();
+
+    //     setUserRepository(repositoryData);
+    //     console.log('userRepository: ', userRepository);
+    //   } catch (error) {
+    //     console.error('Erro ao buscar informações do repositório: ', error);
+    //   }
+    // }
+
     async function fetchUserRepository() {
       try {
-        const repoUrl = `https://api.github.com/repos/${props.userLogin}/rinha-de-backend`;
+        const searchQuery = encodeURIComponent(`rinha-de-backend`);
+        const repoUrl = `https://api.github.com/search/repositories?q=${searchQuery}+user:${props.userLogin}`;
         const response = await fetch(repoUrl);
-        const repositoryData = await response.json();
-
-        setUserRepository(repositoryData);
-        console.log('userRepository: ', userRepository);
+        const searchData = await response.json();
+    
+        if (searchData.items.length > 0) {
+          // Supondo que você deseje pegar o primeiro repositório encontrado
+          const repositoryUrl = searchData.items[0].url;
+          const repositoryResponse = await fetch(repositoryUrl);
+          const repositoryData = await repositoryResponse.json();
+    
+          setUserRepository(repositoryData);
+          console.log('userRepository: ', userRepository);
+        } else {
+          console.log('Nenhum repositório correspondente encontrado');
+        }
       } catch (error) {
         console.error('Erro ao buscar informações do repositório: ', error);
       }
-    }
+    }      
 
     fetchUserRepository();
   }, [props.userLogin]);
