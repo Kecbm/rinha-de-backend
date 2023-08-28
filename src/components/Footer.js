deb143ca244cbf4eafed0c8e7fe04763f9f398fc
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-28 20:16:45 -0300

    :hammer: refatorando o Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+import '../css/Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+    <div>
+        <h3>Francisco Zanfranceschi</h3>
+        <a href="https://github.com/zanfranceschi" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/github.png" alt="GitHub"/>
+        </a>
+        <a href="https://twitter.com/zanfranceschi" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/twitter--v1.png" alt="Twitter"/>
+        </a>
+        <a href="https://www.linkedin.com/in/francisco-zanfranceschi/" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/linkedin.png" alt="Linkedin"/>
+        </a>
+        <a href="https://dev.to/zanfranceschi" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/windows/96/FFFFFF/dev.png" alt="Dev.to"/>
+        </a>
+        <a href="https://www.youtube.com/@zanfranceschi/videos" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/youtube--v1.png" alt="Youtube"/>
+        </a>
+    </div>
+
+    <div>
+        <h3>Klecianny Melo</h3>
+        <a href="https://github.com/Kecbm" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/github.png" alt="GitHub"/>
+        </a>
+        <a href="https://twitter.com/Kecbm" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/twitter--v1.png" alt="Twitter"/>
+        </a>
+        <a href="https://www.linkedin.com/in/kecbm/" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/linkedin.png" alt="Linkedin"/>
+        </a>
+        <a href="https://dev.to/kecbm" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/windows/96/FFFFFF/dev.png" alt="Dev.to"/>
+        </a>
+    </div>
+
+    <div>
+        <h3>Repositório</h3>
+        <a href="https://github.com/zanfranceschi/rinha-de-backend-2023-q3" target="_blank" rel="noopener noreferrer">
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/github.png" alt="GitHub"/>
+        </a>
+    </div>
+
+    <div>
+        <h3>Live</h3>
+        <img width="50" height="50" src="https://img.icons8.com/ios-glyphs/90/FFFFFF/youtube--v1.png" alt="Youtube"/>
+    </div>
+
+    <div>
+        <h1 id="icon-chicken-footer">🐓</h1>
+    </div>
+    </footer>
+  );
+}
+
+export default Footer;
