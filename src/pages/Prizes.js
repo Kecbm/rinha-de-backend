b3680952ce8b559c0e2851e7ee7bd8a2aaf66605
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-28 23:09:32 -0300

    :construction: criando a ágina Prizes

---
+++ src/pages/Prizes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Prizes.css';
+
+function Prizes() {
+  return (
+    <div id="prizes">
+        <h1>Prizes</h1>
+    </div>
+  );
+}
+
+export default Prizes;
