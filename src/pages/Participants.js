aef16d7d46263932af0e1fccffa8cda74f99556c
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-28 23:08:56 -0300

    :construction: criando a página de participantes

---
+++ src/pages/Participants.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Participants.css';
+
+function Participants() {
+  return (
+    <div id="participants">
+        <h1>Participants</h1>
+    </div>
+  );
+}
+
+export default Participants;
