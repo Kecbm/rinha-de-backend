1b5f50bd449b4749638b707117f0cde332531d5a
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-28 23:09:18 -0300

    :construction: criando a página Podium

---
+++ src/pages/Podium.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Podium.css';
+
+function Podium() {
+  return (
+    <div id="podium">
+        <h1>Podium</h1>
+    </div>
+  );
+}
+
+export default Podium;
