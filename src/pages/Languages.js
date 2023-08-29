acb0aac25e06de2e8f2f672604a7b9e6ad83f766
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-28 23:08:39 -0300

    :construction: criando a página Linguagens

---
+++ src/pages/Languages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Languages.css';
+
+function Languages() {
+  return (
+    <div id="languages">
+        <h1>Languages</h1>
+    </div>
+  );
+}
+
+export default Languages;
