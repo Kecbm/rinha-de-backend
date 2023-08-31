95704e3fe652ac3fd6554d02ab8948bdfbae789d
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-30 22:32:27 -0300

    :hammer: componente de Loading da página

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Loading.css';
+
+function Loading() {
+  return (
+    <div className="loading-page">
+        <img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/chicken.png" alt="chicken"/>
+        <img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/eggs.png" alt="eggs"/>
+        <img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/sunny-side-up-eggs.png" alt="sunny-side-up-eggs"/>
+    </div>
+  );
+}
+
+export default Loading;
