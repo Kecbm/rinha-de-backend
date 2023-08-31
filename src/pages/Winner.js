0ca9ee001cfde4306612633baf4b0f37e0bfa8d9
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-30 22:32:47 -0300

    :sparkles: primeira versão do card do vencedor

--- src/pages/Winner.js
+++ src/pages/Winner.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../components/Loading';
 import podiumRanking from '../data/podiumRanking';
 import '../css/Winner.css';
 
@@ -11,6 +12,14 @@ import '../css/Winner.css';
 // }
 
 function Winner({ forks }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 3500); 
+  }, []);
+
   const userLogin = podiumRanking[0].login;
 
   const searchUserData = (userLogin) => {
@@ -24,24 +33,37 @@ function Winner({ forks }) {
 
   return (
     <div id="winner">
-        {
-            podiumRanking.map((user, index) => (
-                <div key={index} id="card-winner">
-                    <p><strong>VENCEDOR</strong></p>
-                    <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer">
-                        <img src={userWinner.owner.avatar_url} alt={user.login}  id="winner-img" />
-                    </a>
-                    <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer" id="winner-login">
-                        {user.login}
-                    </a>
-                    <div id="winner-infos">
-                        <p><strong>{user.language}</strong></p>
-                        <p><strong>Contagem de pessoas:</strong> {user.peopleCount}</p>
-                        <p><strong>P99:</strong> {user.p99}</p>
-                    </div>
-                </div>
-            ))
-        }
+      {
+        isLoading ? (<Loading />) : (
+          podiumRanking.map((user, index) => (
+            <div key={index} id="card-winner">
+              <div id="trophy">
+                <img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/trophy.png" alt="trophy" />
+              </div>
+              <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer">
+                <img src={userWinner.owner.avatar_url} alt={user.login}  id="winner-img" />
+              </a>
+              <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer" id="winner-login">
+                {user.login}
+              </a>
+              <div id="winner-infos">
+                <p>
+                  <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/code-file.png" alt="code-file"/>
+                  <strong>{user.language}</strong>
+                </p>
+                <p>
+                  <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/user-group-man-man.png" alt="user-group-man-man"/>
+                  <strong>Contagem de pessoas:</strong> {user.peopleCount}
+                </p>
+                <p>
+                  <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/statistics--v1.png" alt="statistics--v1"/>
+                  <strong>P99:</strong> {user.p99}
+                </p>
+              </div>
+            </div>
+          ))
+        )
+      }
     </div>
   );
 }
