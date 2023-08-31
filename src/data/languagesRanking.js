e42b93d8088a352da58c99f89dc5dc8a0eeedada
Klecianny Melo <kleciannymelo@gmail.com>
2023-08-30 22:51:39 -0300

    :construction: iniciando a página Linguagens

--- src/data/languagesRanking.js
+++ src/data/languagesRanking.js
@@ -1,14 +1,74 @@
 const languagesRanking = [
     {
-        language: 'Rust',
+        name: 'Rust',
         position: 1,
-        quantityProjects: ''
+        // quantityProjects: ''
     },
     {
-        language: 'Go',
+        name: 'Go',
         position: 2,
-        quantityProjects: ''
+        // quantityProjects: ''
     },
+    {
+        name: '.NET',
+        position: 3,
+        // quantityProjects: ''
+    },
+    {
+        name: 'C++',
+        position: 5,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Elixir',
+        position: 11,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Node',
+        position: 13,
+        // quantityProjects: ''
+    },
+    {
+        name: 'PHP',
+        position: 17,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Java',
+        position: 18,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Ruby on Rails',
+        position: 19,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Python',
+        position: 21,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Gleam',
+        position: 22,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Deno',
+        position: 27,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Kotlin',
+        position: 44,
+        // quantityProjects: ''
+    },
+    {
+        name: 'Bash',
+        position: 51,
+        // quantityProjects: ''
+    }
 ]
 
 export default languagesRanking;
--- src/pages/Languages.js
+++ src/pages/Languages.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../components/Loading';
+import languagesRanking from '../data/languagesRanking';
 import '../css/Languages.css';
 
 function Languages() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 3500); 
+  }, []);
+
   return (
     <div id="languages">
-        <h1>Languages</h1>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          { 
+            languagesRanking.map((language, index) => (
+              <div key={index} className="language-card">
+                <h2 className="language-position">{language.position}</h2>
+                <h1 className="language-name">{language.name}</h1>
+              </div>
+            ))
+          }
+        </div>
+      )}
     </div>
   );
 }
