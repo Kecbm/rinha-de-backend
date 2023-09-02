626d957cca1f34ae5ab5fd813e5b0198796c04c2
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-02 17:50:54 -0300

    :construction: começando a montar o array de participantes

--- src/data/usersRanking.js
+++ src/data/usersRanking.js
@@ -1,9 +1,473 @@
 const usersRanking = [
     {
         position: 1,
-        userLogin: 'viniciusfonseca',
+        login: 'viniciusfonseca',
+        peopleCount: 44936,
+        P99: 17418,
+        language: 'Rust'
+    },
+    {
+        position: 2,
+        login: 'h4nkb31f0ng',
+        peopleCount: 44270,
+        P99: 16405,
+        language: 'Go'
+    },
+    {
+        position: 3,
+        login: 'grupo-2a',
+        peopleCount: 44100,
+        P99: 17588,
+        language: '.NET'
+    },
+    {
+        position: 4,
+        login: 'isadora-souza',
+        peopleCount: 42122,
+        P99: 55570,
+        language: 'Go'
+    },
+    {
+        position: 5,
+        login: 'sofia_aripiprazole',
+        peopleCount: 	40938,
+        P99: 62753,
+        language: 'C++'
+    },
+    {
+        position: 6,
+        login: 'vimsos',
+        peopleCount: 39996,
+        P99: 2799,
+        language: 'Rust'
+    },
+    {
+        position: 7,
+        login: 'jrodrigues',
+        peopleCount: 39070,
+        P99: 44045,
+        language: 'Go'
+    },
+    {
+        position: 8,
+        login: 'lpicanco',
+        peopleCount: 37693,
+        P99: 28739,
+        language: 'Rust'
+    },
+    {
+        position: 9,
+        login: 'navarro',
+        peopleCount: 37567,
+        P99: 18183,
+        language: 'Rust'
+    },
+    {
+        position: 10,
+        login: 'navarro-touche',
+        peopleCount: 35865,
+        P99: 12773,
+        language: 'Rust'
+    },
+    {
+        position: 11,
+        login: 'oliveigah',
+        peopleCount: 34809,
+        P99: 	47416,
+        language: 'Elixir'
+    },
+    {
+        position: 12,
+        login: 'lucaswilliameufrasio',
+        peopleCount: 34680,
+        P99: 1127,
+        language: 'C++'
+    },
+    {
+        position: 13,
+        login: 'luucaspole',
+        peopleCount: 34165,
+        P99: 48510,
+        language: 'Node'
+    },
+    {
+        position: 14,
+        login: 'met4tron',
+        peopleCount: 27904,
+        P99: 	59707,
+        language: 'Node'
+    },
+    {
+        position: 15,
+        login: 'rode',
+        peopleCount: 26607,
+        P99: 2674,
+        language: 'Rust'
+    },
+    {
+        position: 16,
+        login: 'saiintbrisson',
+        peopleCount: 26567,
+        P99: 3454,
+        language: 'Rust'
+    },
+    {
+        position: 17,
+        login: 'lauroappelt',
+        peopleCount: 25493,
+        P99: 21533,
+        language: 'PHP'
+    },
+    {
+        position: 18,
+        login: 'brunoborges',
+        peopleCount: 25352,
+        P99: 44867,
+        language: 'Quartus 3'
+    },
+    {
+        position: 19,
+        login: 'lazaronixon',
+        peopleCount: 24466,
+        P99: 3097,
+        language: 'Ruby'
+    },
+    {
+        position: 20,
+        login: 'leandronsp',
+        peopleCount: 24418,
+        P99: 54741,
+        language: 'Ruby'
+    },
+    {
+        position: 21,
+        login: 'iancambrea',
+        peopleCount: 23831,
+        P99: 19279,
+        language: 'Python'
+    },
+    {
+        position: 22,
+        login: 'thelinuxlich',
+        peopleCount: 22762,
+        P99: 25409,
+        language: 'Gleam'
+    },
+    {
+        position: 23,
+        login: 'luanpontes100',
+        peopleCount: 21315,
+        P99: 54779,
+        language: 'Go'
+    },
+    {
+        position: 24,
+        login: 'lauroappeltv2',
+        peopleCount: 16554,
+        P99: 13854,
+        language: 'PHP'
+    },
+    {
+        position: 25,
+        login: 'true_eduardo',
+        peopleCount: 16415,
+        P99: 7893,
+        language: 'Go'
+    },
+    {
+        position: 26,
+        login: 'rodrigoknol',
+        peopleCount: 16265,
+        P99: 59210,
+        language: 'Node'
+    },
+    {
+        position: 27,
+        login: 'korodzi',
+        peopleCount: 16217,
+        P99: 5178,
+        language: 'Nest'
+    },
+    {
+        position: 28,
+        login: 'lucasteles',
+        peopleCount: 16074,
+        P99: 20162,
+        language: 'C#'
+    },
+    {
+        position: 29,
+        login: 'MarcosCostaDev',
+        peopleCount: 13368,
+        P99: 17304,
+        language: '.NET'
+    },
+    {
+        position: 30,
+        login: 'boaglio',
+        peopleCount: 12957,
+        P99: 59724,
+        language: 'Java'
+    },
+    {
+        position: 31,
+        login: 'viniciusferraz-nativo',
+        peopleCount: 12247,
+        P99: 5879,
+        language: 'Rust'
+    },
+    {
+        position: 32,
+        login: 'andrelsmelo',
+        peopleCount: 11933,
+        P99: 25576,
+        language: 'Node'
+    },
+    {
+        position: 33,
+        login: 'viniciusferraz',
+        peopleCount: 11521,
+        P99: 7916,
         language: ''
-    }
+    },
+    {
+        position: 34,
+        login: 'OpenCodeCo',
+        peopleCount: 10960,
+        P99: 20236,
+        language: 'PHP'
+    },
+    {
+        position: 35,
+        login: 'EuFountai',
+        peopleCount: 10549,
+        P99: 58238,
+        language: 'Node'
+    },
+    {
+        position: 36,
+        login: 'andrew-vasco',
+        peopleCount: 8869,
+        P99: 59655,
+        language: 'Java'
+    },
+    {
+        position: 37,
+        login: 'willy-r',
+        peopleCount: 8458,
+        P99: 36359,
+        language: 'Python'
+    },
+    {
+        position: 38,
+        login: 'gustavocs789',
+        peopleCount: 8002,
+        P99: 18608,
+        language: 'Node'
+    },
+    {
+        position: 39,
+        login: 'juniorleaoo',
+        peopleCount: 7761,
+        P99: 58901,
+        language: 'Java'
+    },
+    {
+        position: 40,
+        login: 'dscamargo',
+        peopleCount: 7503,
+        P99: 16511,
+        language: 'Go'
+    },
+    {
+        position: 41,
+        login: 'bpaulino0',
+        peopleCount: 7351,
+        P99: 37202,
+        language: 'Elixir'
+    },
+    {
+        position: 42,
+        login: 'wesleynepo',
+        peopleCount: 7320,
+        P99: 21249,
+        language: 'Go'
+    },
+    {
+        position: 43,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
+    {
+        position: 0,
+        login: '',
+        peopleCount: 0,
+        P99: 0,
+        language: ''
+    },
 ];
 
 export default usersRanking;
