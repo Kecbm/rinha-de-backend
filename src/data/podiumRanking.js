f7f3bcd9c1388af51c17a0c3d8bbad0f8bbf3288
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-04 22:49:03 -0300

    :sparkles: array do podium top 10

--- src/data/podiumRanking.js
+++ src/data/podiumRanking.js
@@ -5,6 +5,69 @@ const podiumRanking = [
         peopleCount: 44936,
         p99: 17418,
         language: 'Rust'
+    },
+    {
+        position: 2,
+        login: 'h4nkb31f0ng',
+        peopleCount: 44270,
+        p99: 16405,
+        language: 'Go'
+    },
+    {
+        position: 3,
+        login: 'grupo-2a',
+        peopleCount: 44100,
+        p99: 17588,
+        language: '.NET'
+    },
+    {
+        position: 4,
+        login: 'isadora-souza',
+        peopleCount: 42122,
+        p99: 55570,
+        language: 'Go'
+    },
+    {
+        position: 5,
+        login: 'sofia_aripiprazole',
+        peopleCount: 	40938,
+        p99: 62753,
+        language: 'C++'
+    },
+    {
+        position: 6,
+        login: 'vimsos',
+        peopleCount: 39996,
+        p99: 2799,
+        language: 'Rust'
+    },
+    {
+        position: 7,
+        login: 'jrodrigues',
+        peopleCount: 39070,
+        p99: 44045,
+        language: 'Go'
+    },
+    {
+        position: 8,
+        login: 'lpicanco',
+        peopleCount: 37693,
+        p99: 28739,
+        language: 'Rust'
+    },
+    {
+        position: 9,
+        login: 'navarro',
+        peopleCount: 37567,
+        p99: 18183,
+        language: 'Rust'
+    },
+    {
+        position: 10,
+        login: 'navarro-touche',
+        peopleCount: 35865,
+        p99: 12773,
+        language: 'Rust'
     }
 ];
 
