6a7542ddc8144fbd14e1bbb5d393ae839e30355d
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-04 22:48:52 -0300

    :sparkles: array de usuários

--- src/data/usersRanking.js
+++ src/data/usersRanking.js
@@ -3,471 +3,535 @@ const usersRanking = [
         position: 1,
         login: 'viniciusfonseca',
         peopleCount: 44936,
-        P99: 17418,
+        p99: 17418,
         language: 'Rust'
     },
     {
         position: 2,
         login: 'h4nkb31f0ng',
         peopleCount: 44270,
-        P99: 16405,
+        p99: 16405,
         language: 'Go'
     },
     {
         position: 3,
         login: 'grupo-2a',
         peopleCount: 44100,
-        P99: 17588,
+        p99: 17588,
         language: '.NET'
     },
     {
         position: 4,
         login: 'isadora-souza',
         peopleCount: 42122,
-        P99: 55570,
+        p99: 55570,
         language: 'Go'
     },
     {
         position: 5,
         login: 'sofia_aripiprazole',
         peopleCount: 	40938,
-        P99: 62753,
+        p99: 62753,
         language: 'C++'
     },
     {
         position: 6,
         login: 'vimsos',
         peopleCount: 39996,
-        P99: 2799,
+        p99: 2799,
         language: 'Rust'
     },
     {
         position: 7,
         login: 'jrodrigues',
         peopleCount: 39070,
-        P99: 44045,
+        p99: 44045,
         language: 'Go'
     },
     {
         position: 8,
         login: 'lpicanco',
         peopleCount: 37693,
-        P99: 28739,
+        p99: 28739,
         language: 'Rust'
     },
     {
         position: 9,
         login: 'navarro',
         peopleCount: 37567,
-        P99: 18183,
+        p99: 18183,
         language: 'Rust'
     },
     {
         position: 10,
         login: 'navarro-touche',
         peopleCount: 35865,
-        P99: 12773,
+        p99: 12773,
         language: 'Rust'
     },
     {
         position: 11,
         login: 'oliveigah',
         peopleCount: 34809,
-        P99: 	47416,
+        p99: 	47416,
         language: 'Elixir'
     },
     {
         position: 12,
         login: 'lucaswilliameufrasio',
         peopleCount: 34680,
-        P99: 1127,
+        p99: 1127,
         language: 'C++'
     },
     {
         position: 13,
         login: 'luucaspole',
         peopleCount: 34165,
-        P99: 48510,
+        p99: 48510,
         language: 'Node'
     },
     {
         position: 14,
         login: 'met4tron',
         peopleCount: 27904,
-        P99: 	59707,
+        p99: 	59707,
         language: 'Node'
     },
     {
         position: 15,
         login: 'rode',
         peopleCount: 26607,
-        P99: 2674,
+        p99: 2674,
         language: 'Rust'
     },
     {
         position: 16,
         login: 'saiintbrisson',
         peopleCount: 26567,
-        P99: 3454,
+        p99: 3454,
         language: 'Rust'
     },
     {
         position: 17,
         login: 'lauroappelt',
         peopleCount: 25493,
-        P99: 21533,
+        p99: 21533,
         language: 'PHP'
     },
     {
         position: 18,
         login: 'brunoborges',
         peopleCount: 25352,
-        P99: 44867,
-        language: 'Quartus 3'
+        p99: 44867,
+        language: 'Java'
     },
     {
         position: 19,
         login: 'lazaronixon',
         peopleCount: 24466,
-        P99: 3097,
+        p99: 3097,
         language: 'Ruby'
     },
     {
         position: 20,
         login: 'leandronsp',
         peopleCount: 24418,
-        P99: 54741,
+        p99: 54741,
         language: 'Ruby'
     },
     {
         position: 21,
         login: 'iancambrea',
         peopleCount: 23831,
-        P99: 19279,
+        p99: 19279,
         language: 'Python'
     },
     {
         position: 22,
         login: 'thelinuxlich',
         peopleCount: 22762,
-        P99: 25409,
+        p99: 25409,
         language: 'Gleam'
     },
     {
         position: 23,
         login: 'luanpontes100',
         peopleCount: 21315,
-        P99: 54779,
+        p99: 54779,
         language: 'Go'
     },
     {
         position: 24,
         login: 'lauroappeltv2',
         peopleCount: 16554,
-        P99: 13854,
+        p99: 13854,
         language: 'PHP'
     },
     {
         position: 25,
         login: 'true_eduardo',
         peopleCount: 16415,
-        P99: 7893,
+        p99: 7893,
         language: 'Go'
     },
     {
         position: 26,
         login: 'rodrigoknol',
         peopleCount: 16265,
-        P99: 59210,
+        p99: 59210,
         language: 'Node'
     },
     {
         position: 27,
         login: 'korodzi',
         peopleCount: 16217,
-        P99: 5178,
+        p99: 5178,
         language: 'Nest'
     },
     {
         position: 28,
         login: 'lucasteles',
         peopleCount: 16074,
-        P99: 20162,
+        p99: 20162,
         language: 'C#'
     },
     {
         position: 29,
         login: 'MarcosCostaDev',
         peopleCount: 13368,
-        P99: 17304,
+        p99: 17304,
         language: '.NET'
     },
     {
         position: 30,
         login: 'boaglio',
         peopleCount: 12957,
-        P99: 59724,
+        p99: 59724,
         language: 'Java'
     },
     {
         position: 31,
         login: 'viniciusferraz-nativo',
         peopleCount: 12247,
-        P99: 5879,
+        p99: 5879,
         language: 'Rust'
     },
     {
         position: 32,
         login: 'andrelsmelo',
         peopleCount: 11933,
-        P99: 25576,
+        p99: 25576,
         language: 'Node'
     },
     {
         position: 33,
         login: 'viniciusferraz',
         peopleCount: 11521,
-        P99: 7916,
+        p99: 7916,
         language: ''
     },
     {
         position: 34,
         login: 'OpenCodeCo',
         peopleCount: 10960,
-        P99: 20236,
+        p99: 20236,
         language: 'PHP'
     },
     {
         position: 35,
         login: 'EuFountai',
         peopleCount: 10549,
-        P99: 58238,
+        p99: 58238,
         language: 'Node'
     },
     {
         position: 36,
         login: 'andrew-vasco',
         peopleCount: 8869,
-        P99: 59655,
+        p99: 59655,
         language: 'Java'
     },
     {
         position: 37,
         login: 'willy-r',
         peopleCount: 8458,
-        P99: 36359,
+        p99: 36359,
         language: 'Python'
     },
     {
         position: 38,
         login: 'gustavocs789',
         peopleCount: 8002,
-        P99: 18608,
+        p99: 18608,
         language: 'Node'
     },
     {
         position: 39,
         login: 'juniorleaoo',
         peopleCount: 7761,
-        P99: 58901,
+        p99: 58901,
         language: 'Java'
     },
     {
         position: 40,
         login: 'dscamargo',
         peopleCount: 7503,
-        P99: 16511,
+        p99: 16511,
         language: 'Go'
     },
     {
         position: 41,
         login: 'bpaulino0',
         peopleCount: 7351,
-        P99: 37202,
+        p99: 37202,
         language: 'Elixir'
     },
     {
         position: 42,
         login: 'wesleynepo',
         peopleCount: 7320,
-        P99: 21249,
+        p99: 21249,
         language: 'Go'
     },
     {
         position: 43,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'ftsuda',
+        peopleCount: 6951,
+        p99: 60299,
+        language: 'Java'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 44,
+        login: 'fernandozanutto',
+        peopleCount: 6233,
+        p99: 39774,
+        language: 'Kotlin'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 45,
+        login: 'reonardoleis',
+        peopleCount: 5844,
+        p99: 8810,
+        language: 'Go'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 46,
+        login: 'giovannibassi',
+        peopleCount: 5658,
+        p99: 58745,
+        language: '.NET'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 47,
+        login: 'Bandolin',
+        peopleCount: 5205,
+        p99: 16210,
+        language: 'PHP'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 48,
+        login: 'cleciusjm',
+        peopleCount: 3720,
+        p99: 50390,
+        language: 'Kotlin'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 49,
+        login: 'Pr3d4dor-php-puro',
+        peopleCount: 3002,
+        p99: 59348,
+        language: 'PHP'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 50,
+        login: 'wendryo',
+        peopleCount: 2835,
+        p99: 44839,
+        language: 'Node'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        position: 51,
+        login: 'leandronsp-bash',
+        peopleCount: 17,
+        p99: 47482,
+        language: 'Bash'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'kalogs-c',
+        p99: 59594,
+        language: 'Clojure'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'felipemarkson',
+        p99: 58617,
+        language: 'C'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'h4ad',
+        p99: 54593,
+        language: 'Go'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'dupla-de-2',
+        p99: 51942, language:
+        'Java'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'marcospaulo',
+        p99: 50418,
+        language: 'Java'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'LuisKpBeta',
+        p99: 41978,
+        language: 'Go'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'Bandolin_simplified_api',
+        p99: 40267,
+        language: 'PHP'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'isaacnborges',
+        p99: 40090,
+        language: '.NET'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'Pr3d4dor-laravel',
+        p99: 24781,
+        language: 'PHP'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'sinhorinho',
+        p99: 23474,
+        language: 'Elixir'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'guimeira',
+        p99: 20227,
+        language: 'Java'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'vhogemann',
+        p99: 20054,
+        language: 'F#'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'CaravanaCloud',
+        p99: 19377,
+        language: '.NET'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'gustmrg',
+        p99: 16880,
+        language: '.NET'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'Tagliatti',
+        p99: 16077,
+        language: '.NET'
     },
     {
-        position: 0,
-        login: '',
-        peopleCount: 0,
-        P99: 0,
-        language: ''
+        login: 'lucasnribeiro',
+        p99: 11740,
+        language: 'TypeScript'
     },
+    {
+        login: 'krymancer',
+        p99: 10231,
+        language: 'Rust'
+    },
+    {
+        login: 'brahma',
+        p99: 5759,
+        language: 'Bash'
+    },
+    {
+        login: 'lucasmadeira',
+        p99: 5720,
+        language: 'Java'
+    },
+    {
+        login: 'rwillians',
+        p99: 245,
+        language: 'Elixir'
+    },
+    {
+        login: 'alberto_souza',
+        language: 'Java'
+    },
+    {
+        login: 'allan-cordeiro',
+        language: 'Go'
+    },
+    {
+        login: 'andre237',
+        language: 'Java'
+    },
+    {
+        login: 'carlosdaniiel07',
+        language: '.NET'
+    },
+    {
+        login: 'danielfireman',
+        language: 'Go'
+    },
+    {
+        login: 'davidlins',
+        language: 'Java'
+    },
+    {
+        login: 'fabricio_juliatto',
+        language: 'Rust'
+    },
+    {
+        login: 'flavio1110',
+        language: 'Go'
+    },
+    {
+        login: 'hampshire',
+        language: 'Haskell'
+    },
+    {
+        login: 'igorsantos07',
+        language: 'PHP'
+    },
+    {
+        login: 'insalubre',
+        language: 'V'
+    },
+    {
+        login: 'lucasraziel',
+        language: 'Node'
+    },
+    {
+        login: 'matheuslc',
+        language: 'Go'
+    },
+    {
+        login: 'MrPowerGamerBR',
+        language: 'Kotlin'
+    },
+    {
+        login: 'natanaelsimoes',
+        language: 'Node'
+    },
+    {
+        login: 'ramoncunha',
+        language: 'Java'
+    },
+    {
+        login: 'rodrigograudo',
+        language: 'Nest'
+    },
+    {
+        login: 'uasouz',
+        language: '.NET'
+    },
+    {
+        login: 'willian',
+        language: 'Go'
+    }
 ];
 
 export default usersRanking;
