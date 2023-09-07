7874b00f4e8909ac665e859baffd318b0ad6ca73
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-06 22:58:31 -0300

    :sparkles: aplicando animação no card do winner

--- src/pages/Winner.js
+++ src/pages/Winner.js
@@ -3,14 +3,6 @@ import Loading from '../components/Loading';
 import podiumRanking from '../data/podiumRanking';
 import '../css/Winner.css';
 
-// {
-//     position: 1,
-//     login: 'viniciusfonseca',
-//     peopleCount: 44936,
-//     p99: 17418,
-//     language: 'Rust'
-// }
-
 function Winner({ forks }) {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -20,6 +12,8 @@ function Winner({ forks }) {
     }, 3500); 
   }, []);
 
+  const winnerData = podiumRanking[0];
+  console.log('winnerData: ', winnerData);
   const userLogin = podiumRanking[0].login;
 
   const searchUserData = (userLogin) => {
@@ -35,33 +29,31 @@ function Winner({ forks }) {
     <div id="winner">
       {
         isLoading ? (<Loading />) : (
-          podiumRanking.map((user, index) => (
-            <div key={index} id="card-winner">
-              <div id="trophy">
-                <img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/trophy.png" alt="trophy" />
-              </div>
-              <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer">
-                <img src={userWinner.owner.avatar_url} alt={user.login}  id="winner-img" />
-              </a>
-              <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer" id="winner-login">
-                {user.login}
-              </a>
-              <div id="winner-infos">
-                <p>
-                  <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/code-file.png" alt="code-file"/>
-                  <strong>{user.language}</strong>
-                </p>
-                <p>
-                  <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/user-group-man-man.png" alt="user-group-man-man"/>
-                  <strong>Contagem de pessoas:</strong> {user.peopleCount}
-                </p>
-                <p>
-                  <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/statistics--v1.png" alt="statistics--v1"/>
-                  <strong>P99:</strong> {user.p99}
-                </p>
-              </div>
+          <div key={winnerData.login} id="card-winner">
+            <div id="trophy">
+              <img width="94" height="94" src="https://img.icons8.com/3d-fluency/94/trophy.png" alt="trophy" />
             </div>
-          ))
+            <a href={`https://github.com/${winnerData.login}`} target="_blank" rel="noopener noreferrer">
+              <img src={userWinner.owner.avatar_url} alt={winnerData.login}  id="winner-img" />
+            </a>
+            <a href={`https://github.com/${winnerData.login}`} target="_blank" rel="noopener noreferrer" id="winner-login">
+              {winnerData.login}
+            </a>
+            <div id="winner-infos">
+              <p>
+                <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/code-file.png" alt="code-file"/>
+                <strong>{winnerData.language}</strong>
+              </p>
+              <p>
+                <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/user-group-man-man.png" alt="user-group-man-man"/>
+                <strong>Contagem de pessoas:</strong> {winnerData.peopleCount}
+              </p>
+              <p>
+                <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/896c0a/statistics--v1.png" alt="statistics--v1"/>
+                <strong>P99:</strong> {winnerData.p99}
+              </p>
+            </div>
+          </div>
         )
       }
     </div>
