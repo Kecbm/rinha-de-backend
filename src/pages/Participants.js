dae357054845e549a070efeba89ac1f71bd79f89
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-06 22:59:33 -0300

    :construction: continuando a página Participantes

--- src/pages/Participants.js
+++ src/pages/Participants.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../components/Loading';
+import usersRanking from '../data/usersRanking';
 import '../css/Participants.css';
 
-function Participants() {
+// {
+//   position: 1,
+//   login: 'viniciusfonseca',
+//   peopleCount: 44936,
+//   p99: 17418,
+//   language: 'Rust'
+// }
+
+function Participants({ forks }) {
+  const [isLoading, setIsLoading] = useState(true);
+  console.log('forks: ', forks);
+
+  useEffect(() => {
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 5000); 
+  }, []);
+
+  const usersRankingNormalized = usersRanking.map((user) => {
+    const userFork = forks.find((fork) => fork.owner.login === user.login);
+
+    return {
+      ...user,
+      userFork
+    };
+  });
+  
+  console.log('usersRankingNormalized: ', usersRankingNormalized);
+
   return (
     <div id="participants">
-        <h1>Participants</h1>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div id="home-content">
+        {usersRankingNormalized.map((user) => (
+          <div key={user.position} className="card-user">
+            <a href={user.userFork.owner.html_url} target="_blank" rel="noopener noreferrer">
+              <img src={user.userFork.owner.avatar_url} alt={user.userFork.owner.login}  className="user-img" />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className="user-login">
+              {user.login}
+            </a>
+          </div>
+        ))}
+      </div>
+      )}
     </div>
   );
 }
