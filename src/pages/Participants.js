8ee14951877f9135ff3ceea62ab90e46f10ea5ca
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-18 16:51:53 -0300

    :construction: continuando a estilização dos cards da página Participants

--- src/pages/Participants.js
+++ src/pages/Participants.js
@@ -18,7 +18,7 @@ function Participants() {
         isLoading ? (<Loading />) : (
             usersRanking.map((user) => (
             <div key={user.id} className="card-participant">
-              <h1>{user.position}</h1>
+              <h1 className="participant-position">{user.position}°</h1>
               <a href={`https://github.com/${user.login}`} target="_blank" rel="noopener noreferrer">
                 <img src={`https://github.com/${user.login}.png`} alt={`Foto do usuário ${user.login}`} className="img-participant" />
               </a>
@@ -27,12 +27,14 @@ function Participants() {
               </a>
               <div className="participant-infos">
                 <p>
-                  <strong>Contagem de pessoas:</strong> {user.peopleCount}
+                  <img width="35" height="35" src="https://img.icons8.com/ios-filled/50/03a3b7/code-file.png" alt="code-file" className="card-participant-icon"/>{user.language}
                 </p>
                 <p>
-                  <strong>P99:</strong> {user.p99}
+                  <img width="35" height="35" src="https://img.icons8.com/ios-filled/50/03a3b7/user-group-man-man.png" alt="user-group-man-man" className="card-participant-icon"/><strong>Contagem de pessoas:</strong> {user.peopleCount}
                 </p>
-                <p>{user.language}</p>
+                <p>
+                  <img width="35" height="35" src="https://img.icons8.com/ios-filled/50/03a3b7/statistics--v1.png" alt="statistics--v1" className="card-participant-icon"/><strong>P99:</strong> {user.p99}
+                </p>
               </div>
             </div>
             ))
