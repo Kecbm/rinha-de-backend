9ed7c770444b3b11ce0c5952391e8bed3f075494
Klecianny Melo <kleciannymelo@gmail.com>
2023-09-18 21:06:24 -0300

    :sparkles: adicionando grau a posição do usuário

--- src/data/usersRanking.js
+++ src/data/usersRanking.js
@@ -1,356 +1,356 @@
 const usersRanking = [
     {
-        position: 1,
+        position: '1°',
         login: 'viniciusfonseca',
         peopleCount: 44936,
         p99: 17418,
         language: 'Rust'
     },
     {
-        position: 2,
+        position: '2°',
         login: 'leorcvargas',
         peopleCount: 44270,
         p99: 16405,
         language: 'Go'
     },
     {
-        position: 3,
+        position: '3°',
         login: 'andr3marra',
         peopleCount: 44100,
         p99: 17588,
         language: '.NET'
     },
     {
-        position: 4,
+        position: '4°',
         login: 'isadoramsouza',
         peopleCount: 42122,
         p99: 55570,
         language: 'Go'
     },
     {
-        position: 5,
+        position: '5°',
         login: 'aripiprazole',
         peopleCount: 	40938,
         p99: 62753,
         language: 'C++'
     },
     {
-        position: 6,
+        position: '6°',
         login: 'vimsos',
         peopleCount: 39996,
         p99: 2799,
         language: 'Rust'
     },
     {
-        position: 7,
+        position: '7°',
         login: 'jrodrigues',
         peopleCount: 39070,
         p99: 44045,
         language: 'Go'
     },
     {
-        position: 8,
+        position: '8°',
         login: 'lpicanco',
         peopleCount: 37693,
         p99: 28739,
         language: 'Rust'
     },
     {
-        position: 9,
+        position: '9°',
         login: 'navarro',
         peopleCount: 37567,
         p99: 18183,
         language: 'Rust'
     },
     {
-        position: 10,
+        position: '10°',
         login: 'reu',
         peopleCount: 35865,
         p99: 12773,
         language: 'Rust'
     },
     {
-        position: 11,
+        position: '11°',
         login: 'oliveigah',
         peopleCount: 34809,
         p99: 	47416,
         language: 'Elixir'
     },
     {
-        position: 12,
+        position: '12°',
         login: 'lucaswilliameufrasio',
         peopleCount: 34680,
         p99: 1127,
         language: 'C++'
     },
     {
-        position: 13,
+        position: '13°',
         login: 'lukas8219',
         peopleCount: 34165,
         p99: 48510,
         language: 'Node'
     },
     {
-        position: 14,
+        position: '14°',
         login: 'met4tron',
         peopleCount: 27904,
         p99: 	59707,
         language: 'Node'
     },
     {
-        position: 15,
+        position: '15°',
         login: 'rode',
         peopleCount: 26607,
         p99: 2674,
         language: 'Rust'
     },
     {
-        position: 16,
+        position: '16°',
         login: 'saiintbrisson',
         peopleCount: 26567,
         p99: 3454,
         language: 'Rust'
     },
     {
-        position: 17,
+        position: '17°',
         login: 'lauroappelt',
         peopleCount: 25493,
         p99: 21533,
         language: 'PHP'
     },
     {
-        position: 18,
+        position: '18°',
         login: 'brunoborges',
         peopleCount: 25352,
         p99: 44867,
         language: 'Java'
     },
     {
-        position: 19,
+        position: '19°',
         login: 'lazaronixon',
         peopleCount: 24466,
         p99: 3097,
         language: 'Ruby'
     },
     {
-        position: 20,
+        position: '20°',
         login: 'leandronsp',
         peopleCount: 24418,
         p99: 54741,
         language: 'Ruby'
     },
     {
-        position: 21,
+        position: '21°',
         login: 'iancambrea',
         peopleCount: 23831,
         p99: 19279,
         language: 'Python'
     },
     {
-        position: 22,
+        position: '22°',
         login: 'thelinuxlich',
         peopleCount: 22762,
         p99: 25409,
         language: 'Gleam'
     },
     {
-        position: 23,
+        position: '23°',
         login: 'luanpontes100',
         peopleCount: 21315,
         p99: 54779,
         language: 'Go'
     },
     {
-        position: 24,
+        position: '24°',
         login: 'lauroappelt',
         peopleCount: 16554,
         p99: 13854,
         language: 'PHP'
     },
     {
-        position: 25,
+        position: '25°',
         login: 'eduwr',
         peopleCount: 16415,
         p99: 7893,
         language: 'Go'
     },
     {
-        position: 26,
+        position: '26°',
         login: 'rodrigoknol',
         peopleCount: 16265,
         p99: 59210,
         language: 'Node'
     },
     {
-        position: 27,
+        position: '27°',
         login: 'korodzi',
         peopleCount: 16217,
         p99: 5178,
         language: 'Nest'
     },
     {
-        position: 28,
+        position: '28°',
         login: 'lucasteles',
         peopleCount: 16074,
         p99: 20162,
         language: 'C#'
     },
     {
-        position: 29,
+        position: '29°',
         login: 'MarcosCostaDev',
         peopleCount: 13368,
         p99: 17304,
         language: '.NET'
     },
     {
-        position: 30,
+        position: '30°',
         login: 'boaglio',
         peopleCount: 12957,
         p99: 59724,
         language: 'Java'
     },
     {
-        position: 31,
+        position: '31°',
         login: 'viniciusfcf',
         peopleCount: 12247,
         p99: 5879,
         language: 'Rust'
     },
     {
-        position: 32,
+        position: '32°',
         login: 'andrelsmelo',
         peopleCount: 11933,
         p99: 25576,
         language: 'Node'
     },
     {
-        position: 33,
+        position: '33°',
         login: 'viniciusferraz',
         peopleCount: 11521,
         p99: 7916,
         language: ''
     },
     {
-        position: 34,
+        position: '34°',
         login: 'OpenCodeCo',
         peopleCount: 10960,
         p99: 20236,
         language: 'PHP'
     },
     {
-        position: 35,
+        position: '35°',
         login: 'eletroswing',
         peopleCount: 10549,
         p99: 58238,
         language: 'Node'
     },
     {
-        position: 36,
+        position: '36°',
         login: 'Andrewsoares15',
         peopleCount: 8869,
         p99: 59655,
         language: 'Java'
     },
     {
-        position: 37,
+        position: '37°',
         login: 'willy-r',
         peopleCount: 8458,
         p99: 36359,
         language: 'Python'
     },
     {
-        position: 38,
+        position: '38°',
         login: 'GustavoCesarSantos',
         peopleCount: 8002,
         p99: 18608,
         language: 'Node'
     },
     {
-        position: 39,
+        position: '39°',
         login: 'juniorleaoo',
         peopleCount: 7761,
         p99: 58901,
         language: 'Java'
     },
     {
-        position: 40,
+        position: '40°',
         login: 'dscamargo',
         peopleCount: 7503,
         p99: 16511,
         language: 'Go'
     },
     {
-        position: 41,
+        position: '41°',
         login: 'brunojppb',
         peopleCount: 7351,
         p99: 37202,
         language: 'Elixir'
     },
     {
-        position: 42,
+        position: '42°',
         login: 'wesleynepo',
         peopleCount: 7320,
         p99: 21249,
         language: 'Go'
     },
     {
-        position: 43,
+        position: '43°',
         login: 'ftsuda',
         peopleCount: 6951,
         p99: 60299,
         language: 'Java'
     },
     {
-        position: 44,
+        position: '44°',
         login: 'fernandozanutto',
         peopleCount: 6233,
         p99: 39774,
         language: 'Kotlin'
     },
     {
-        position: 45,
+        position: '45°',
         login: 'reonardoleis',
         peopleCount: 5844,
         p99: 8810,
         language: 'Go'
     },
     {
-        position: 46,
+        position: '46°',
         login: 'giggio',
         peopleCount: 5658,
         p99: 58745,
         language: '.NET'
     },
     {
-        position: 47,
+        position: '47°',
         login: 'Bandolin',
         peopleCount: 5205,
         p99: 16210,
         language: 'PHP'
     },
     {
-        position: 48,
+        position: '48°',
         login: 'cleciusjm',
         peopleCount: 3720,
         p99: 50390,
         language: 'Kotlin'
     },
     {
-        position: 49,
+        position: '49°',
         login: 'Pr3d4dor',
         peopleCount: 3002,
         p99: 59348,
         language: 'PHP'
     },
     {
-        position: 50,
+        position: '50°',
         login: 'wendryo',
         peopleCount: 2835,
         p99: 44839,
         language: 'Node'
     },
     {
-        position: 51,
+        position: '51°',
         login: 'leandronsp',
         peopleCount: 17,
         p99: 47482,
